fd2ad57900907d36f56b61e46fa5076f8776ab1b
JSkahl <josekahl333@gmail.com>
2024-11-29 20:49:02 -0300

    feat: terminando a estilização da pagina desktop

--- src/router/index.js
+++ src/router/index.js
@@ -38,7 +38,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('../views/LoginView.vue'), 
     },
   ],
 })
