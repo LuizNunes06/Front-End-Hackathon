430505778b38c623573265c7fdfb42f39b106749
IsadoraAlcantara <Isadoraalca405@gmail.com>
2024-11-29 16:06:18 -0300

    FEAT: criação da nav, add de cor no base.css e criação de pasta de icones

---
+++ src/components/icons/index.js
@@ -0,0 +1,11 @@
+import AlertBoxOutline from 'vue-material-design-icons/AlertBoxOutline.vue';
+import AccountGroupOutline from 'vue-material-design-icons/AccountGroupOutline.vue';
+import History from 'vue-material-design-icons/History.vue';
+import Menu from 'vue-material-design-icons/Menu.vue';
+
+export {
+    AlertBoxOutline,
+    AccountGroupOutline,
+    History,
+    Menu,
+}
