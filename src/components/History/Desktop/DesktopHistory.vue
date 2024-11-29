4a487c1d95534f5888c79b49acfd374f1639d028
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-11-29 18:22:33 -0300

    CHORE: ajuste no espaçamento do body e correção no nome de pasta

---
+++ src/components/History/Desktop/DesktopHistory.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HistoryContainer from "./HistoryContainer.vue";
+import AgendaDesktop from "../../LayoutComponents/DesktopLayout/AgendaDesktop.vue";
+</script>
+<template>
+  <div class="conteiner-conteudo">
+    <HistoryContainer />
+    <AgendaDesktop />
+  </div>
+</template>
+
+<style scoped>
+.conteiner-conteudo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
