5c751fb8fc513a889d5cb3fcbc52c98c87a92172
anaclara0603 <anaclara.hahn1@gmail.com>
2024-11-30 05:15:45 -0300

    FEAT: Correção css turma

--- src/views/ConsultaTurmaView.vue
+++ src/views/ConsultaTurmaView.vue
@@ -1,11 +1,12 @@
 <script setup>
-import AsideDesktop from "@/components/LayoutComponents/DesktopLayout/AsideDesktop.vue";
-import HeaderDesktop from "@/components/LayoutComponents/DesktopLayout/HeaderDesktop.vue";
+// import AsideDesktop from "@/components/LayoutComponents/DesktopLayout/AsideDesktop.vue";
+// import HeaderDesktop from "@/components/LayoutComponents/DesktopLayout/HeaderDesktop.vue";
 
 import {DefaultTurma} from "../components";
 </script>
 <template>
-  <main>
     <DefaultTurma/>
-  </main>
 </template>
+
+<style scoped>
+</style>
