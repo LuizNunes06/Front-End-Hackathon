535b9c827035309fdd2dd4b1288398c54170541d
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-11-30 00:55:00 -0300

    REFACTOR:  Remoção de rota repetida e correção em importação faltante

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import DefaultOccurrences from './Occurrences/DefaultOccurrences.vue';
 import DefaultHistory from "./History/DefaultHistory.vue";
 import DefaultTurma from "./ConsultaTurma/DefaultTurma.vue";
-import AgendaDesktop from './LayoutComponents/DesktopLayout/AgendaDesktop.vue';
 import AsideDesktop from './LayoutComponents/DesktopLayout/AsideDesktop.vue';
 import HeaderDesktop from './LayoutComponents/DesktopLayout/HeaderDesktop.vue';
 import DefaultStudent from "./Student/DefaultStudent.vue";
@@ -9,7 +8,7 @@ import DefaultStudent from "./Student/DefaultStudent.vue";
 export {
     DefaultHistory,
     DefaultOccurrences,
-    AgendaDesktop,
+    DefaultTurma,
     AsideDesktop,
     HeaderDesktop,
     DefaultStudent,
